339a7dd768371003927bf1e89c2fa5c2b94f63f5
Bryce Mecum <petridish@gmail.com>
2019-02-23 12:14:37 -0900

    Fix next/sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const withSass = require('@zeit/next-sass')
 
-
-module.exports = {
-  ...withSass(),
+module.exports = withSass({
   target: 'serverless',
   env: {
     api_base: 'https://cn-stage.test.dataone.org/cn/v2/',
     debounce: 300
   }
-}
+});
