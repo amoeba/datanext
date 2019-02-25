74be44dc7c53407ed3e1edb9152e56dedaacd59c
Bryce Mecum <petridish@gmail.com>
2019-02-24 17:50:13 -0900

    Enable ssr with a basic express.js server

---
+++ server.js
@@ -0,0 +1,23 @@
+/*
+* Adapted from 
+* https://github.com/zeit/next.js/blob/master/examples/custom-server/server.js
+*/
+
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
