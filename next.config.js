b6d1737837592f0ae1ab2dfeae67d8aeecf1d4ac
Bryce Mecum <petridish@gmail.com>
2019-03-06 21:44:30 -0900

    Point at production CN index

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   target: 'serverless',
   env: {
-    api_base: 'https://cn-stage.test.dataone.org/cn/v2/',
+    api_base: 'https://search.dataone.org/cn/v2/',
     debounce: 300
   }
 });
