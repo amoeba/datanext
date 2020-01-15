347446c3c11a4ebbd62b3786ac2bf060eac13553
Bryce Mecum <petridish@gmail.com>
2020-01-15 01:32:37 -0900

    Broken rewrite

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
-import React from "react";
+import useSWR from 'swr'
+import fetch from 'unfetch'
 
-import CustomHead from '../components/custom_head';
-import Header from "../components/header";
-import Search from "../components/search";
+import SearchResult from '../components/SearchResult.js'
 
-import "../style.css"
+const key = '*:*';
 
-export default class extends React.Component {
-  render() {
-    return (
-      <div>
-        <CustomHead>
-          <title>Search</title>
-        </CustomHead>
-        <Header />
-        <Search
-          appData={this.props.appData} />
-      </div>
-    );
-  }
+const fetcher = async (path) => {
+  const url = 'https://search.dataone.org/cn/v2/query/solr/?q=' +
+    path +
+    '+AND+formatType:METADATA+AND+-obsoletedBy:*' +
+    '&sort=dateUploaded+desc' +
+    '&fl=identifier,dateUploaded' +
+    '&wt=json';
+
+  return fetch(url).then(r => r.json());
 }
+
+Index.getInitialProps = async () => {
+  const data = await fetcher(key);
+
+  return { data };
+}
+
+function Index() {
+  const initialData = props.data;
+  const { data, error } = useSWR(key, fetcher, { initialData })
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  return (<ul>
+    { data.response.docs.map(d => SearchResult(d)) }
+  </ul>)
+}
+
+export default Index
