8a1403df00cacdb5cd8703d83127bddcae4e5b6a
Bryce Mecum <petridish@gmail.com>
2020-02-10 18:45:42 -0900

    Cleanup

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,20 @@
-import React from "react";
 import Layout from "../components/Layout";
 
-export default class extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <div>
-          <p>
-            This is just a simple demo of a DataONE search web client written in
-            Next.js. See{" "}
-            <a href="https://github.com/amoeba/datanext">
-              https://github.com/amoeba/datanext
-            </a>{" "}
-            for more info.
-          </p>
-        </div>
-      </Layout>
-    );
-  }
+function About() {
+  return (
+    <Layout>
+      <div>
+        <p>
+          This is just a simple demo of a DataONE search web client written in
+          Next.js. See{" "}
+          <a href="https://github.com/amoeba/datanext">
+            https://github.com/amoeba/datanext
+          </a>{" "}
+          for more info.
+        </p>
+      </div>
+    </Layout>
+  );
 }
+
+export default About;
--- pages/profile.js
+++ pages/profile.js
@@ -1,28 +1,11 @@
 import React from "react";
+
 import Layout from "../components/Layout";
 import TokenConext from "../shared/TokenContext";
-
 import TokenButton from "../components/TokenButton";
 
 export default class extends React.Component {
   static contextType = TokenConext;
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loginURL: ""
-    };
-  }
-
-  componentDidMount() {
-    // TODO: Factor out into config
-    this.setState({
-      loginURL:
-        "https://cn.dataone.org/portal/oauth?action=start&target=" +
-        encodeURIComponent(window.location.href)
-    });
-  }
 
   render() {
     return (
