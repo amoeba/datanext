c7ee7c836c67b45d6c81510b8e5c4f0ab6cae23f
Bryce Mecum <petridish@gmail.com>
2020-10-28 00:31:08 -0800

    Formatting

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,11 @@ export async function getServerSideProps(context) {
 export default function Index({ results }) {
   const items = results.map((item) =>
     <div key={item.id}>
-      <p><Link href={"/package/" + encodeURIComponent(item.id)}><a><strong>{item.title}</strong>. <br />By {item.origin.join(", ")}</a></Link></p>
+      <p>
+        <Link href={"/package/" + encodeURIComponent(item.id)}>
+          <a><strong>{item.title}</strong>. <br />By {item.origin.join(", ")}</a>
+        </Link>
+      </p>
     </div >
   );
 
