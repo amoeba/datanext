123a84041e627ff1457d43579901fd5dfaaa9e56
Bryce Mecum <petridish@gmail.com>
2020-10-28 22:58:14 -0800

    Swap ids for filenames in packagetabel

--- components/PackageTable.js
+++ components/PackageTable.js
@@ -1,14 +1,14 @@
 export default function PackageTable({ members }) {
   const rows = members.map(member =>
     <tr key={member.identifier}>
-      <td>{member.identifier}</td>
+      <td>{member.fileName}</td>
       <td>{member.formatId}</td>
       <td>{member.size}</td>
     </tr>)
   return <table className="package">
     <thead>
       <tr>
-        <th>ID</th>
+        <th>Name</th>
         <th>Format</th>
         <th>Size</th>
       </tr>
