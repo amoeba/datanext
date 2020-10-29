62b2feb6bd9f57c06137cdb72f6d40846188e3b9
Bryce Mecum <petridish@gmail.com>
2020-10-28 23:00:28 -0800

    Add notice when a packagetable is empty

--- components/PackageTable.js
+++ components/PackageTable.js
@@ -1,10 +1,15 @@
 export default function PackageTable({ members }) {
+  if (members.length === 0) {
+    return <div>This package has no files associated with it. Check the metadata below for links to files that may exist at other locations.</div>
+  }
+
   const rows = members.map(member =>
     <tr key={member.identifier}>
       <td>{member.fileName}</td>
       <td>{member.formatId}</td>
       <td>{member.size}</td>
     </tr>)
+
   return <table className="package">
     <thead>
       <tr>
