f465b8e488d89f1d111922cfb9a5904666b0358b
Bryce Mecum <petridish@gmail.com>
2020-11-19 00:01:19 -0900

    Prettify app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,31 @@
 import Link from "next/link";
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
-  return <div>
-
-    <div id="top">
-      Sign In
+  return (
+    <div>
+      <div id="top">Sign In</div>
+      <header>
+        <Link href="/">
+          <a>
+            <h1>DataONE</h1>
+          </a>
+        </Link>
+        <nav>
+          <Link href="/search">
+            <a>Search</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </nav>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer></footer>
     </div>
-    <header>
-      <Link href="/"><a>
-        <h1>DataONE</h1></a>
-      </Link>
-      <nav>
-        <Link href="/search"><a>Search</a></Link>
-        <Link href="/about"><a>About</a></Link>
-      </nav>
-    </header>
-    <main>
-      <Component {...pageProps} />
-    </main>
-    <footer></footer>
-  </div>;
+  );
 }
 
-export default App
+export default App;
