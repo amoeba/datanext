68dbd2823944aabd86718ba3554850aa88af8054
Bryce <petridish@gmail.com>
2021-04-23 11:17:44 -0800

    Add noopener to package links

--- components/PackageTableItem.js
+++ components/PackageTableItem.js
@@ -3,6 +3,6 @@ export default function PackageTableItem(item) {
         <td>{item.fileName}</td>
         <td>{item.formatId}</td>
         <td>{item.size}</td>
-        <td><a className="button" href={item.dataUrl}>Download</a></td>
+        <td><a className="button" rel="noopener" href={item.dataUrl}>Download</a></td>
     </tr>
 }
