97f25af7fbf1a2704cef2ab498a33af1800bbd7c
Bryce Mecum <petridish@gmail.com>
2021-10-02 21:33:35 -0800

    Fix Next.js-specific issue with meta-viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react";
+import Head from "next/head"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import "../styles/globals.css";
@@ -7,6 +8,9 @@ import { StoreProvider } from "../lib/store"
 function App({ Component, pageProps }) {
   return (
     <StoreProvider>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <Header />
       <main>
         <Component {...pageProps} />
