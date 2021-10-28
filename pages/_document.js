f69ff1df25c1db600061fbec759af3a48441c34c
Bryce Mecum <petridish@gmail.com>
2021-10-27 18:59:07 -0800

    Add a viewport meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="description" content="DataNext Search Catalog" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="preconnect" href="https://search.dataone.org" />
           <link rel="preconnect" href="https://cn.dataone.org" />
         </Head>
