e64457360acbb187f7b3ba012fab2dcc97eac8a9
Bryce Mecum <petridish@gmail.com>
2022-06-08 18:49:54 -0800

    Add text to search result icon

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,38 +1,62 @@
 import { useContext } from "react";
 import Link from "next/link";
-import { StoreContext } from "../lib/store"
+import { StoreContext } from "../lib/store";
 
 function getBestNodeName(nodes, datasource) {
-  const idx = nodes.findIndex(e => e.identifier === datasource)
+  const idx = nodes.findIndex((e) => e.identifier === datasource);
 
   if (idx === -1) {
-    return datasource
+    return datasource;
   }
 
-  return nodes[idx].name
+  return nodes[idx].name;
 }
 
 export default function SearchResult({ index, doc }) {
-  const radius = 30
+  const radius = 30;
 
-  const zebra = index % 2 ? "search-result-even" : "search-resultodd"
+  const zebra = index % 2 ? "search-result-even" : "search-resultodd";
 
-  const { nodes } = useContext(StoreContext)
+  const { nodes } = useContext(StoreContext);
 
   // Produce best node name
   const nodeName = getBestNodeName(nodes[0], doc.datasource);
 
-  return <div className={"search-result " + zebra}>
-    <div className="search-result-icon">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width={radius * 2} height={radius * 2} viewBox={"0 0 " + radius * 2 + " " + radius * 2}>
-        <circle r="50%" cx="50%" cy="50%" fill="rgba(200, 200, 200, 0.75)" />
-      </svg>
+  return (
+    <div className={"search-result " + zebra}>
+      <div className="search-result-icon">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          width={radius * 2}
+          height={radius * 2}
+          viewBox={"0 0 " + radius * 2 + " " + radius * 2}
+        >
+          <circle r="50%" cx="50%" cy="50%" fill="rgba(200, 200, 200, 0.75)" />
+          <text
+            fill="rgba(120, 120, 120, 0.75)"
+            font-size="200%"
+            text-anchor="middle"
+            alignment-baseline="central"
+            x="50%"
+            y="50%"
+            font-family="Menlo, Monaco, monospace"
+          >
+            {doc.datasource.replace("urn:node:", "").substring(0, 1)}
+          </text>
+        </svg>
+      </div>
+      <div className="search-result-text">
+        <Link href={"/package/" + encodeURIComponent(doc.id)}>
+          <a>
+            {doc.origin.join(", ")}. {doc.pubDate.substr(0, 4)}.{" "}
+            <span className="search-result-text-title">{doc.title}</span>.{" "}
+            {nodeName}.{" "}
+            <span className="search-result-text-identifier">{doc.id}</span>
+          </a>
+        </Link>
+        {!doc.isPublic ? "🔐" : ""}
+      </div>
     </div>
-    <div className="search-result-text">
-      <Link href={"/package/" + encodeURIComponent(doc.id)}>
-        <a>{doc.origin.join(", ")}. {doc.pubDate.substr(0, 4)}. <span className="search-result-text-title">{doc.title}</span>. {nodeName}. <span className="search-result-text-identifier">{doc.id}</span></a>
-      </Link>
-      {!doc.isPublic ? "🔐" : ""}
-    </div>
-  </div>
+  );
 }
