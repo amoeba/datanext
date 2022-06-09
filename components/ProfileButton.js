91dc26515cab925d7a318abeab6b4c41981e4622
Bryce Mecum <petridish@gmail.com>
2022-06-08 19:50:29 -0800

    Prettier ProfileButton.js

--- components/ProfileButton.js
+++ components/ProfileButton.js
@@ -1,34 +1,38 @@
 import { useEffect, useState, useContext } from "react";
 import Link from "next/link";
 import { cn, portal_path } from "../lib/api";
-import { StoreContext } from "../lib/store"
+import { StoreContext } from "../lib/store";
 
 export default function ProfileButton() {
   const [href, setHref] = useState(null);
 
   useEffect(() => {
     async function updateHref() {
-      setHref(cn + portal_path + window.location.href)
+      setHref(cn + portal_path + window.location.href);
     }
 
     updateHref();
-  }, [])
+  }, []);
 
-  const { isLoggedIn, name } = useContext(StoreContext)
+  const { isLoggedIn, name } = useContext(StoreContext);
 
-  let content
+  let content;
 
   if (isLoggedIn[0]) {
-    content = <Link href="/profile">
-      <a>{name[0]}</a>
-    </Link>
+    content = (
+      <Link href="/profile">
+        <a>{name[0]}</a>
+      </Link>
+    );
   } else if (href && !isLoggedIn[0]) {
-    content = <Link href={href}><a>Sign In</a></Link>
+    content = (
+      <Link href={href}>
+        <a>Sign In</a>
+      </Link>
+    );
   } else {
-    content = "Loading..."
+    content = "Loading...";
   }
 
-  return <div>
-    {content}
-  </div>
+  return <div>{content}</div>;
 }
