f5e1cbf32aa12050698808f0232b25e04b2877e6
Bryce Mecum <petridish@gmail.com>
2022-06-08 18:27:22 -0800

    Tweak header (colors mainly)

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,25 @@
-import Link from "next/link"
-import ProfileButton from "./ProfileButton"
+import Link from "next/link";
+import ProfileButton from "./ProfileButton";
 
 export default function Header() {
-  return <header>
-    <div>
-      <Link href="/">
-        <a>
-          <h1>DataONE</h1>
-        </a>
-      </Link>
-    </div>
-    <nav>
-      <Link href="/">
-        <a>Search</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <ProfileButton></ProfileButton>
-    </nav>
-  </header>
+  return (
+    <header>
+      <div>
+        <Link href="/">
+          <a>
+            <h1>DataNext</h1>
+          </a>
+        </Link>
+      </div>
+      <nav>
+        <Link href="/">
+          <a>Search</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <ProfileButton></ProfileButton>
+      </nav>
+    </header>
+  );
 }
